1525b1b0b970326b14cccee5720017ed5a0ef8af
rharris529 <jordandine@gmail.com>
2022-01-16 16:55:48 -0600

    added confirm prompts for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,36 @@
 // Assignment code here
-var num = "0123456789";
-var lowerCase = "abcdefghijklmnoprstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specInput =  "!@#$%^&*()";
+var chars = "0123456789abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var passwordLength = window.prompt("How many characters would you like your password to have?");
+  console.log(passwordLength); 
+
+  if (passwordLength < 8) {
+    window.alert("Your password must contain at least 8 characters. Try again.");
+    return;
+  };
+
+  if (passwordLength < 128) {
+    window.alert("Your password cannot contain more than 128 characters. Try again.");
+    return;
+  }
+  
+  var confirmNum = window.confirm("Please confirm if you would like your password to contain numeric characters.");
+  console.log(confirmNum);
+
+  var confirmLower = window.confirm("Please confirm if you would like your password to include lowercase letters.");
+  console.log(confirmLower);
+
+  var confirmUpper = window.confirm("Please confirm if you would like your password to include uppercase letters.");
+  console.log(confirmUpper);
+
+  var confirmSpecial = window.confirm("Please confirm if you would like your password to include special characters.");
+  console.log(confirmSpecial);
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -17,6 +42,8 @@ function writePassword() {
     
 
 }
+
+generatePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
